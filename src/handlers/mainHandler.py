05099cdde035b6efd6d0d63f1e6d34b239bdcbb9
ziyodullo <ziyodullodev@gmail.com>
2025-04-05 13:09:18 +0500

    add: some news

--- src/handlers/mainHandler.py
+++ src/handlers/mainHandler.py
@@ -1,8 +1,16 @@
 from aiogram import Dispatcher
+from dotenv import load_dotenv
 from locales.localizator import Localizator
+from functions.DatabaseSqlite3 import SQLiteDatabase as Database
+# from functions.Database import Database
+import os
+load_dotenv()
+SQLITE3_NAME = os.getenv('SQLITE3_NAME')
 
 localizator = Localizator()
 dp = Dispatcher()
+db = Database(db_path=SQLITE3_NAME)
+
 from handlers.startCommandHander import message_router
 
 async def mainHandler(bot):
